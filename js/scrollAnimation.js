ef6c0eff16c16b58101b64b2988e97fb7f793df4
Kevin Boyle <papershack@gmail.com>
2019-06-12 20:49:13 -0700

    testing scroll animations

--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -1,25 +1,24 @@
 
-//  var $animation_elements = $('.card');
-// // var $window = $(window);
+ var $animation_elements = $('.card');
+var $window = $(window);
 
-// function check_if_in_view() {
-//   var windowHeight = $(window).height();
-//   var scrollTop = $(window).scrollTop();
+function check_if_in_view() {
+  var window_height = $window.height();
+  var scrollTop = $window.scrollTop();
 
-//   $('.card').each(function() {
-//     var $el = $(this);
-//     // var element_height = $el.outerHeight();
-//     var elTopPosition = $el.getBoundingClientRect().top();
-//     console.log($el, "el top pos: ", elTopPosition, " windowHeight: ", windowHeight)
-//     var distance = elTopPosition < windowHeight * 1.5 && elTopPosition > windowHeight * 2;
-//     if (distance){
-//       $el.addClass('in-view') 
-//     } else {
-//       $el.removeClass('in-view')
-//     }
+  $('.card').each(function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var elementTopPosition = $element.offset().top;
+    var distance = scrollTop - elementTopPosition;
+    if (distance > -150 && distance < 15000){
+      $element.addClass('in-view') 
+    } else {
+      $element.removeClass('in-view')
+    }
 
-//   });
-// }
+  });
+}
 
-// $(window).on('scroll resize', check_if_in_view);
-// $(window).trigger('scroll');
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
