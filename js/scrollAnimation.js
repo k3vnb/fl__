eb781f8d0f6128be9023dce2031c27c85913aade
Kevin Boyle <papershack@gmail.com>
2019-06-13 00:14:13 -0700

    scroll animation performs as needed

--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -1,24 +1,24 @@
 
- var $animation_elements = $('.card');
-var $window = $(window);
+const $window = $(window);
 
 function check_if_in_view() {
-  var window_height = $window.height();
-  var scrollTop = $window.scrollTop();
+  const scrollTop = $window.scrollTop();
 
-  $('.card').each(function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var elementTopPosition = $element.offset().top;
-    var distance = scrollTop - elementTopPosition;
-    if (distance > -150 && distance < 15000){
-      $element.addClass('in-view') 
+  $('.tile').each(function() {
+    const el = $(this);
+    // const element_height = el.outerHeight();
+    const elTopPosition = el.offset().top;
+    const distance = scrollTop - elTopPosition;
+    // console.log($element, distance)
+    if (distance > -350 && distance < -90){
+      el.addClass('in-view') 
     } else {
-      $element.removeClass('in-view')
+      el.removeClass('in-view')
     }
 
   });
 }
 
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
+
+  $window.on('scroll resize', check_if_in_view);
+  $window.trigger('scroll');
