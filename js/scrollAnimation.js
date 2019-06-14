b034b13112ea5a4128567852df5730b98157503b
Kevin Boyle <papershack@gmail.com>
2019-06-14 16:56:55 -0700

    clean up scrollAnimation code& stylesheet

--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -1,10 +1,4 @@
 
-// $(window).on('resize', () => {
-//   if (window.matchMedia('(max-width: 700px)').matches){
-//     checkTilePosition
-//   }
-// })
-
 function checkScreenWidth(){
   if (window.matchMedia('(max-width: 700px)').matches){
     $(window).trigger('scroll');
@@ -14,11 +8,7 @@ function checkScreenWidth(){
       $(this).css('transform', 'scale(1)');
     })
   }
-
-  
 }
-
-
 
 function checkTilePosition(){
     const scrollTop = $(window).scrollTop();
@@ -28,7 +18,7 @@ function checkTilePosition(){
       const windowHeight = $(window).height(); 
       const tileTopPosition = tile.offset().top; 
       const tileMiddlePosition = tileTopPosition + tileHeight/2;
-      const standard = 1.09985;
+      const standard = 1.14;
       const middle = tileMiddlePosition - scrollTop - windowHeight/2;
       const distanceFromMiddle = Math.abs(middle);
       tile.css('transform', `scale(${standard - distanceFromMiddle/1000})`)
