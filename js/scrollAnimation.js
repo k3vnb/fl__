d967bfe2844be39c3aec2e6925a421322e09baec
Kevin Boyle <papershack@gmail.com>
2019-06-14 21:01:27 -0700

    create function to calculate screen height and adjust size of container accordingly. It makes first tile centered on Y axis on load in mobile view

--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -1,8 +1,10 @@
 
 function checkScreenWidth(){
+  // these functions are only necessary for mobile view
   if (window.matchMedia('(max-width: 700px)').matches){
     $(window).trigger('scroll');
     $(window).on('scroll load ', checkTilePosition);
+    adjustTileContainerHeight()
   } else {
     $('.tile').each(function() {
       $(this).css('transform', 'scale(1)');
@@ -10,7 +12,22 @@ function checkScreenWidth(){
   }
 }
 
+function adjustTileContainerHeight(){
+  // the tiles will load at the center of the screen's y-axis regardless of window height. This is done by first setting the elements to align properly for approx. 640px screen-height in css, then we are calculating the window height and subtracting the difference
+  windowHeight = $(window).height();
+  let px;
+  if (windowHeight > 640) {
+    px = windowHeight % 640;
+  } else if (windowHeight > 600) {
+    px = 0;
+  } else {
+    px = windowHeight % 500 * -1
+  }
+  $('main').css('height', `calc(233.33vh - ${px}px`)
+}
+
 function checkTilePosition(){
+  // the tiles should get larger as they approach the middle of the screen and smaller as they move away from it. Here we calculate the center of the screen against the middle of the tile and transform scale according to their position.
     const scrollTop = $(window).scrollTop();
     $('.tile').each(function() {
       const tile = $(this);
@@ -21,7 +38,7 @@ function checkTilePosition(){
       const standard = 1.14;
       const middle = tileMiddlePosition - scrollTop - windowHeight/2;
       const distanceFromMiddle = Math.abs(middle);
-      tile.css('transform', `scale(${standard - distanceFromMiddle/1000})`)
+      tile.css('transform', `scale(${standard - distanceFromMiddle/2400})`)
     });
   }
   
